8d1e4633b4ebc53c6dc0e62af96bad72723a40af
SammyKhalaf <sammykhalaf@icloud.com>
2024-04-29 16:02:28 -0500

    Boss

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aedee9cfab9a98bd866e64782d83b73d94f26ee3")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925bf6c4b28119ab088e56313a4d853ee50ba2c1")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
